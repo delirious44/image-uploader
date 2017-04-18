790ca568fa1f8151d3b76e548c2d0e935d9ce65d
Josh Beyer <mydevaccs@gmail.com>
2017-04-18 10:12:35 -0500

    updated gulp file

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var sass = require('gulp-sass');
+ 
+var paths = {  
+  sass: ['./sass/**/*.sass'],
+  pug: ['./pug/**/*.pug']
+};  
+
+
+
+// ##### PUG TO HTML #####
+gulp.task('pug', function() {
+  gulp.src('./pug/.*.pug')
+  .pipe(pug({
+      pretty: true
+  }))
+  .pipe(gulp.dest('./..'))
+});
+
+gulp.task('watch-pug', function(){
+    gulp.watch('./*.pug', ['pug'])
+});
+
+// ##### SASS WATCH #####
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+gulp.task('watch', function() {  
+  gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.pug, ['pug']);
+});
+
+
+gulp.task('default', ['watch']);  
