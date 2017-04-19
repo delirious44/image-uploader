92c536b74df68dd17eaf3e1f5e66dfd9fc2d820a
Josh Beyer <mydevaccs@gmail.com>
2017-04-19 13:39:50 -0500

    Added multer

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require("path");
 //External modules
 var express = require("express");
 var pug = require("pug");
+var multer = require("multer");
 
 //Local modules
 var routes = require("./routes.js");
@@ -11,6 +12,29 @@ var routes = require("./routes.js");
 //Setup
 var app = express();
 var port = process.env.PORT || 3000;
+
+
+
+
+// Multer setup
+var storage = multer.diskStorage({
+    // Set file destination
+    destination: function(req,file,cb){
+        cb(null, 'uploads/images')
+    },
+    // Name the file
+    filename: function(req, file, cb){
+        cb(null, Date.now() + file.originalname);
+    }
+});
+var upload = multer({ storage: storage });
+app.post('/', upload.any(), function(req, res, next){
+		console.log(req.files);
+        res.send(req.files);
+	});
+
+
+
 
 // bodyParser add , note: bodyParser should be near the top - before the routing, the comments can be cleared afterwards
 // npm install body-parser / saved at package.json .. >> code from kastri
